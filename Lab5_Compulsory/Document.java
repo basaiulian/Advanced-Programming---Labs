61f6c3119d4687c0efccdfa2c7584b6a3b42199a
Iulian-Danut Basa <58566863+basaiulian@users.noreply.github.com>
2020-03-20 18:09:38 +0200

    labs

---
+++ Lab5_Compulsory/Document.java
@@ -0,0 +1,78 @@
+package com.company.lab5.compulsory;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Document implements Serializable {
+    private String id;
+    private String name;
+    private String location;
+    private Map<String, Object> tags;
+
+    public Document() {
+        tags = new HashMap<>();
+    }
+
+    public Document(String id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.tags = new HashMap<>();
+    }
+
+    public Document(String id, String name, String location, Map<String, Object> tags) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.tags = tags;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Map<String, Object> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, Object> tags) {
+        this.tags = tags;
+    }
+
+    public void addTag(String key, Object obj) {
+        tags.put(key, obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
+
+    //…
+}
