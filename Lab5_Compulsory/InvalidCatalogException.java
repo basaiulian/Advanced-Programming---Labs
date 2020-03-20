9cc859be04a146a72511300c25ee52a154ca52e8
Iulian-Danut Basa <58566863+basaiulian@users.noreply.github.com>
2020-03-20 18:15:21 +0200

    Labs

--- Lab5_Compulsory/InvalidCatalogException.java
+++
@@ -1,7 +0,0 @@
-package com.company.lab5.compulsory;
-
-public class InvalidCatalogException extends Exception {
-    public InvalidCatalogException(Exception ex) {
-        super("Invalid catalog file.", ex);
-    }
-}
